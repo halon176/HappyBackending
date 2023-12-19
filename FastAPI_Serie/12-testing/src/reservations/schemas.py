f6b80b104a200134b96b4756b7ec107ffdf16e46
halon176 <acc.github@halon.cc>
2023-12-19 01:40:57 +0100

    new video

---
+++ FastAPI_Serie/12-testing/src/reservations/schemas.py
@@ -0,0 +1,24 @@
+from datetime import datetime
+from typing import Optional
+
+from pydantic import PositiveInt, Field
+
+from src.schemas import CustomBase
+
+
+class ReservationResponse(CustomBase):
+    id: PositiveInt = Field(..., examples=[1])
+    num_guests: PositiveInt = Field(..., examples=[10])
+    user_id: PositiveInt = Field(..., examples=[1])
+    event_id: PositiveInt = Field(..., examples=[1])
+    created_at: datetime
+
+
+class ReservationCreate(CustomBase):
+    num_guests: PositiveInt = Field(..., examples=[10])
+    event_id: PositiveInt = Field(..., examples=[1])
+
+
+class ReservationUpdate(CustomBase):
+    event_id: Optional[PositiveInt] = Field(None, examples=[1])
+    num_guests: Optional[PositiveInt] = Field(None, examples=[10])
